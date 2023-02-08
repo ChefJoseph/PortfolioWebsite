87d6fd53a58acd7877d4dbff7f167cc8c2cd79d4
Joseph Hirotsu <jhirotsu@gmail.com>
2023-02-08 14:37:48 -0500

    Router basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <Router basename="https://chefjoseph.github.io/PortfolioWebsite/">
     <App />
   </Router>
 );
