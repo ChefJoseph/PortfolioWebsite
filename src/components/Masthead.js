c845718185eff0fd9e156bd241527f7de4bcd914
Joseph Hirotsu <jhirotsu@gmail.com>
2023-02-09 15:51:07 -0500

    favicon href, masthead p white

--- src/components/Masthead.js
+++ src/components/Masthead.js
@@ -9,9 +9,9 @@ function Masthead() {
                     <h1 className="mx-auto  text-uppercase">Joseph Hirotsu</h1>
                     <h2 className="text-white fs-3 mx-auto mt-2 mb-5">Fullstack Software Engineer</h2>
                     <p className="text-white">Full-stack software engineer experienced in Ruby on Rails and JavaScript based programming, coming from 10+ years of experience as a corporate executive chef.</p>
-                    <p>Throughout my time at Flatiron School, I demonstrated the ability to develop and integrate solutions through individual and team projects with very short deadlines. I often took the lead in team settings to plan wireframes and responsibilities, design components, and map actionable steps to deliver successful results. 
+                    <p className="text-white">Throughout my time at Flatiron School, I demonstrated the ability to develop and integrate solutions through individual and team projects with very short deadlines. I often took the lead in team settings to plan wireframes and responsibilities, design components, and map actionable steps to deliver successful results. 
                     </p>
-                    <p>
+                    <p className="text-white">
                     Feeding my appetite through studying JavaScript, ReactJS, CSS, Data Structures and Algorithms, Python, TypeScript, Web3, AI and current Industry events.</p>
                 </div>
            
